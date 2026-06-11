5219186e643b848eb85383633ee5ec9e1d1b72a5
prashantindy <74930213+prashantindy@users.noreply.github.com>
2026-06-11 11:09:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
+let ApiServer;
 async function init() {
-    console.log('App initialized.');
-    document.getElementById('output').textContent = greet('Prashant');
-    console.log("host: " + window.location.host);
-    console.log("hostname: " + window.location.hostname);
-    console.log("origin: " + window.location.origin);
-    console.log("search: " + window.location.search);
-    const params = new URLSearchParams(window.location.search);
-    const value = params.get('rowId');
-    console.log("rowId: " + value);
     let msg;
-    msg = await getMessage();
-    console.log("message: " + JSON.stringify(msg));
+    let params;
+    let rowId;
+    let datatableId;
+    params = new URLSearchParams(window.location.search);
+    rowId = params.get('rowId');
+    datatableId = params.get('datatableId');
+    ApiServer = params.get('apiServer');
+    msg = await getMessage(rowId, datatableId);
+    // console.log("message: " + JSON.stringify(msg));
+    // document.getElementById('output')!.textContent = greet('Prashant');
+    document.getElementById('output').textContent = JSON.stringify(msg);
 }
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-async function getMessage() {
+// function greet(name: string) {
+//     return `Hello, ${name}!`;
+// }
+async function getMessage(rowId, datatableId) {
     let rtn;
     let headers;
     let config;
-    headers = {};
+    headers = {
+        'rowid': '' + rowId,
+        'datatableid': '' + datatableId
+    };
     config = {
         method: 'GET',
         headers: headers
     };
-    rtn = doFetch(`${window.location.origin}/message`, config);
+    // rtn = doFetch(`${window.location.origin}/message`, config);
+    rtn = doFetch(`${ApiServer}/message`, config);
     return rtn;
 }
 async function doFetch(url, config) {
