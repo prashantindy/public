9ae101db4b6013c363fa088a9ad2df6f65ca3577
prashantindy <74930213+prashantindy@users.noreply.github.com>
2026-06-12 11:59:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,61 @@
-let ApiServer;
+let ApiServer: string | null;
+
 async function init() {
     let msg;
     let params;
     let rowId;
     let datatableId;
+
     params = new URLSearchParams(window.location.search);
     rowId = params.get('rowId');
     datatableId = params.get('datatableId');
     ApiServer = params.get('apiServer');
+
     msg = await getMessage(rowId, datatableId);
-    // console.log("message: " + JSON.stringify(msg));
-    // document.getElementById('output')!.textContent = greet('Prashant');
-    // document.getElementById('output')!.textContent = JSON.stringify(msg);
-    document.getElementById('output').textContent = JSON.stringify(msg, undefined, 2);
+
+    document.getElementById('output')!.textContent = JSON.stringify(msg, undefined, 2);
 }
-// function greet(name: string) {
-//     return `Hello, ${name}!`;
-// }
-async function getMessage(rowId, datatableId) {
+
+function toggleDebug(elementId: string) {
+    document.getElementById(elementId)!.classList.toggle("hidden");
+}
+
+async function getMessage(rowId: string | null, datatableId: string | null) {
     let rtn;
     let headers;
     let config;
+
     headers = {
         'rowid': '' + rowId,
         'datatableid': '' + datatableId
     };
+
     config = {
         method: 'GET',
         headers: headers
     };
-    // rtn = doFetch(`${window.location.origin}/message`, config);
+
     rtn = doFetch(`${ApiServer}/message`, config);
+
     return rtn;
 }
-async function doFetch(url, config) {
+
+async function doFetch(url: string, config: RequestInit) {
     let rtn;
     let res;
+
     res = await fetch(url, config);
+
     if (res.ok) {
         rtn = await res.json();
-    }
-    else {
+    } else {
         throw Error(res.statusText);
     }
+
     return rtn;
 }
+
+(window as any).toggleDebug = toggleDebug;
+(window as any).init = init;
+
 init();
-export {};
