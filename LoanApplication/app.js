4110cfd3f338a273623495c23af42ff403e7c8a9
prashantindy <74930213+prashantindy@users.noreply.github.com>
2026-07-30 16:30:01 -0400

    Add files via upload

---
+++ LoanApplication/app.js
@@ -0,0 +1,47 @@
+let ApiServer;
+async function init() {
+    let msg;
+    let params;
+    let rowId;
+    let datatableId;
+    params = new URLSearchParams(window.location.search);
+    rowId = params.get('rowId');
+    datatableId = params.get('datatableId');
+    ApiServer = params.get('apiServer');
+    msg = await getMessage(rowId, datatableId);
+    document.getElementById('output').textContent = JSON.stringify(msg, undefined, 2);
+}
+function toggleDebug(elementId) {
+    document.getElementById(elementId).classList.toggle("hidden");
+}
+async function getMessage(rowId, datatableId) {
+    let rtn;
+    let headers;
+    let config;
+    headers = {
+        'rowid': '' + rowId,
+        'datatableid': '' + datatableId
+    };
+    config = {
+        method: 'GET',
+        headers: headers
+    };
+    rtn = doFetch(`${ApiServer}/message`, config);
+    return rtn;
+}
+async function doFetch(url, config) {
+    let rtn;
+    let res;
+    res = await fetch(url, config);
+    if (res.ok) {
+        rtn = await res.json();
+    }
+    else {
+        throw Error(res.statusText);
+    }
+    return rtn;
+}
+window.toggleDebug = toggleDebug;
+window.init = init;
+init();
+export {};
